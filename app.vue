b05c332e0af66289aa2522c756747c646dc12972
Мотуз Марина <154107051+Marina8181@users.noreply.github.com>
2025-05-30 12:38:44 +1000

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,21 +1,54 @@
-
 <template>
-  <div>
-    <Header />
-    <NuxtPage />
+  <div class="layout">
+    <Header @toggle-drawer="isDrawerOpen = $event" />
+    <main class="content">
+      <Main />
+      <NuxtPage />
+    </main>
+    <Footer />
+    <Modal :isOpen="isModalOpen" @close="isModalOpen = false" />
+    <Drawer :isOpen="isDrawerOpen" @close="isDrawerOpen = false" />
   </div>
 </template>
 
 <script>
 import Header from '~/components/header/Header.vue'
+import Footer from '~/components/Footer.vue'
+import Modal from '~/components/Modal.vue'
+import Drawer from '~/components/header/Drawer.vue'
+import '@csstools/normalize.css'
 
 export default {
-  components: {
-    Header
+  components: { Header, Footer, Modal, Drawer },
+  data() {
+    return {
+      isModalOpen: false,
+      isDrawerOpen: false
+    }
   }
 }
 </script>
 
 <style>
-/* Глобальные стили */
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+}
+
+.content {
+  flex: 1 0 auto; /* Растягиваем контент на всё доступное пространство */
+}
+
+body.no-scroll {
+  overflow: hidden; /* Блокировка прокрутки при открытых модальных окнах */
+}
+
 </style>
